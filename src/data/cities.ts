57c89b65f0c509ea73b869047cf7d2d67adc2132
willjw3 <will.j.ward3@gmail.com>
2021-05-15 19:11:43 +0900

    Complete refactor

---
+++ src/data/cities.ts
@@ -0,0 +1,39 @@
+interface city {
+    name: string;
+    coordinates: [number, number]
+}
+
+export const cities: city[] = [
+    {
+        name: "New York",
+        coordinates: [-73.9800, 40.7700]
+    },
+    {
+        name: "Washington DC",
+        coordinates: [-77.46, 38.95]
+    },
+    {
+        name: "Miami",
+        coordinates: [-80.28, 25.82]
+    },
+    {
+        name: "Chicago",
+        coordinates: [-87.65, 41.90]
+    },
+    {
+        name: "Houston",
+        coordinates: [-95.35, 29.97]
+    }, 
+    {
+        name: "Seattle",
+        coordinates: [-122.30, 47.45]
+    },
+    {
+        name: "San Francisco",
+        coordinates: [-122.68, 37.75]
+    }, 
+    {
+        name: "Los Angeles",
+        coordinates: [-118.40, 33.93]
+    }
+]
